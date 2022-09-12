abfcbbdf37ef09e958b76f89dab533f806b0f76f
asim sheikh <amazingasim3000@gmail.com>
2022-09-12 22:08:32 +0530

    nothing

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
 let express=require('express');
 let app=express();
-let bodyparser=require('body-parser')
 let port=process.env.PORT || 5000;
 let projectrouter=require('./router/projectroute')
 let dbconnection=require('./config/config');
 let cors= require('cors');
-const multer = require('multer');
-let upload=multer();
-app.use(express.static('views'));
 app.use(express.json());
+
+dbconnection.connect()   //connected to database
 app.use(cors({
     origin:"*",
     methods:['POST','PUT','PATCH','DELETE']
 }))
-
-app.use(upload.array())
-
-
-
-
-dbconnection.connect()   //connected to database
-
-app.get('/',(req,res)=>{res.send('server is on')})
-
+app.use(express.static('views'))
 
 
 app.use('/api/project',projectrouter);
 
+
+
 app.listen(port,()=>{
     console.log('server is mongo mongo');
 })
+
+
--- config/config.js
+++ config/config.js
@@ -1,20 +1,25 @@
 const mongoclient=require('mongodb').MongoClient;
 
 let url='mongodb+srv://AmazingAsim:AmazingAsim3000@amazingasim.tqrgz.mongodb.net/AmazingAsimportfolio?retryWrites=true&w=majority';
+
 let dbclient;
 
 
 //module.exports={connect};
 
 exports.connect=()=>{
+    console.log('connect is called')
     mongoclient.connect(url).then(res=>{
         console.log('mongo is connected')
         dbclient=res;
     }).catch(err=>{
-        console.log(err)
+        console.log('error',err)
     })
 }
+
 
 exports.getcollection=(name)=>{
     return dbclient.db('AmazingAsimportfolio').collection(name);
 }
+
+console.log('someting here ')
